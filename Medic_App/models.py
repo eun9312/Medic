6f1c6a420c4a0e4fc7a7319220c39ddbf93a2889
Won Chung <eun9312@yahoo.co.kr>
2016-03-17 22:23:45 -0400

    model implementation for project specification

--- Medic_App/models.py
+++ Medic_App/models.py
@@ -2,4 +2,24 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.contrib.auth.models import User
+from datetime import datetime
+
 # Create your models here.
+
+class Profile(models.Model):
+	user = models.ForeignKey(User)
+	firstname = models.CharField(max_length=30)
+	lastname = models.CharField(max_length=30)
+	age = models.PositiveSmallIntegerField()
+	picture = models.ImageField(upload_to="", blank=True)
+	status = models.CharField(max_length=30)
+
+class Symptom(models.Model):
+	name = models.CharField(max_length=30)
+
+class Disease(models.Model):
+	name = models.CharField(max_length=30)
+	symptoms = models.ManyToManyField(Symptom, related_name="symptoms")
+	description = models.CharField(max_length=500)
+
